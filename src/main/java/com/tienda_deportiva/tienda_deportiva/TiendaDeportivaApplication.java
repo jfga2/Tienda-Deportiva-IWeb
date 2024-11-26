610638af2eaba259d97a45550663725c12e4c092
jfga2 <71645114+jfga2@users.noreply.github.com>
2024-11-26 16:18:15 +0100

    commit inicial

---
+++ src/main/java/com/tienda_deportiva/tienda_deportiva/TiendaDeportivaApplication.java
@@ -0,0 +1,13 @@
+package com.tienda_deportiva.tienda_deportiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaDeportivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaDeportivaApplication.class, args);
+	}
+
+}
